303c5c0d85a2319f3c8c7b3e9d755bb3fba1fd20
Nayeli Michelle Tipantiza Cumbal <106408982+nayelimtc@users.noreply.github.com>
2025-03-05 00:46:47 -0500

    CorsConfig

---
+++ src/main/java/com/banquito/gateway/facturacion/banquito/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.banquito.gateway.facturacion.banquito.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.addAllowedOrigin("https://front-gateway-seven.vercel.app");
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        source.registerCorsConfiguration("/v1/facturaciones/**", config);
+        source.registerCorsConfiguration("/v1/comisiones/**", config);
+
+        return new CorsFilter(source);
+    }
+}
